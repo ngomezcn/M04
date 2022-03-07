562bff6f163727c068fb539b08e4ac66dbf5a14d
Ubuntu <ubuntu@ubuntu2110.linuxvmimages.local>
2022-03-07 12:51:41 +0100

    .

---
+++ JavaScript/ejercicio2.js
@@ -0,0 +1,16 @@
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max+1);
+}
+
+function generateSecretCode(size) {
+    let array = []
+    for (let index = 0; index < size; index++) {
+        array.push(getRandomInt(10))
+        
+    }
+}
+
+function main() {
+
+}
