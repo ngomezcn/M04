9d4fd20f2436648cc08f60e36d84c3063eaacca5
Your Name <you@example.com>
2022-04-25 09:47:14 +0200

    zz

--- JavaScript/dom/DOMExercici7.js
+++ JavaScript/dom/DOMExercici7.js
@@ -1,3 +1,15 @@
 function ej7() {
+    const tbl = document.getElementById("Taula");
   
-}
+    const y = prompt("Y")
+    const x = prompt("X")
+  
+    for (let i = 0; i < y; i++) {
+      const tr = tbl.insertRow();
+      for (let j = 0; j < x; j++) {
+          const td = tr.insertCell();
+          td.appendChild(document.createTextNode(`${i} - ${j}`));
+      }
+    }
+  }
+  
